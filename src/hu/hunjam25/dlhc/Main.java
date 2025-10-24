030eac0feb002abd07d6d2d88532ecb1049f10db
Turcsányi Tamás <tamas.f.turcsanyi@gmail.com>
2025-10-24 21:52:39 +0200

    jframe

--- src/hu/hunjam25/dlhc/Main.java
+++ src/hu/hunjam25/dlhc/Main.java
@@ -1,8 +1,24 @@
 package hu.hunjam25.dlhc;
 
+import javax.swing.*;
+import java.awt.*;
+
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        var window = new JFrame();
+        window.setTitle("Dont let him cook!");
+        window.add(new JLabel("Hello world"));
+        window.setVisible(true);
+        window.setBounds(10,10,600,500);
+
+        var canvas = new Canvas();
+        window.add(canvas);
+        for(;;){
+            window.repaint();
+        }
+
     }
 }
 

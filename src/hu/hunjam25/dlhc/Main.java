de6c9a181aea9b6fe8085fb18d16faef0e384d6b
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-10-24 21:50:24 +0200

    Add blanket main

--- src/hu/hunjam25/dlhc/Main.java
+++ src/hu/hunjam25/dlhc/Main.java
@@ -1,8 +1,41 @@
 package hu.hunjam25.dlhc;
 
-class Main {
+public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.err.println("Failed to set look and feel");
+        }
+
+        JFrame f = new JFrame("Don't let him cook");
+
+        f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        // f.setIconImage(ImageIO.read(Main.class.getResource("icon.png")));
+        // f.setLayout(new FlowLayout());
+        f.setLocationRelativeTo(null);
+
+        Canvas c = new Canvas();
+        f.add(c);
+
+        f.setVisible(true);
+
+        while (true) {
+            BufferStrategy bs = c.getBufferStrategy();
+            if (bs == null) {
+                c.createBufferStrategy(2);
+                continue;
+            }
+
+            Graphics2D g2d = (Graphics2D) bs.getDrawGraphics();
+
+            draw();
+
+            g2d.dispose();
+
+            // sleep
+        }
     }
 }
 

e93219bfff81aad1fad8b59c012f1d0d1e13b2e2
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-10-25 11:24:22 +0200

    Add minigame example

---
+++ src/hu/hunjam25/dlhc/gameplay/ChoppingBoard.java
@@ -0,0 +1,16 @@
+package hu.hunjam25.dlhc.gameplay;
+
+import java.awt.Graphics2D;
+
+public class ChoppingBoard extends Minigame {
+
+    @Override
+    public void tick(float dt) {
+        super.tick(dt);
+    }
+
+    @Override
+    public void render(Graphics2D gd) {
+        super.render(gd);
+    }
+}

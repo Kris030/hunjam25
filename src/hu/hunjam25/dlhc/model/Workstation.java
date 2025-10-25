307a41a19310ab02b41c0d99f1b90a3ca54139d2
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-10-25 16:47:12 +0200

    Keep track of workers at station

--- src/hu/hunjam25/dlhc/model/Workstation.java
+++ src/hu/hunjam25/dlhc/model/Workstation.java
@@ -14,37 +14,41 @@ public class Workstation extends GameObject {
         Oven,
         Fridge,
         Stove,
-
         ;
-
     }
+
     private Sprite sprite;
     public WorkstationType type = WorkstationType.Stove;
 
     // relative to GameObject position
     Point.Float workingOffset;
 
+    public int workers = 0;
+
     public Workstation(WorkstationType type, Point.Float position, Point.Float workingOffset) {
         this.type = type;
         this.position = position;
         this.workingOffset = workingOffset;
 
-
         this.sprite = new Sprite(AssetManager.getImage(type.name()));
-        //sprite.scaleToTileMax();
+        // sprite.scaleToTileMax();
         sprite.scaleWidth();
         float sc = sprite.getRatio();
-        //System.out.println(sc);
-        if (sc < 1)
-            this.position.y += (1f - (1f/sc))/2f;
-        else
-            this.position.y -= (1f - (1f/sc))/2f;
-    }
+        // System.out.println(sc);
 
+        if (sc < 1)
+            this.position.y += (1f - (1f / sc)) / 2f;
+        else
+            this.position.y -= (1f - (1f / sc)) / 2f;
+    }
 
     @Override
     public void render(Graphics2D gd) {
         super.render(gd);
         sprite.render(gd);
+    }
+
+    public boolean hasWorker() {
+        return workers > 0;
     }
 }

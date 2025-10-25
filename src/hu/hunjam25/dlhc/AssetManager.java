ffb9e443dc54c3b3cca402e72af0fdbb039a7e1b
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2025-10-25 14:45:21 +0200

    Sound storage + img cleanup

--- src/hu/hunjam25/dlhc/AssetManager.java
+++ src/hu/hunjam25/dlhc/AssetManager.java
@@ -1,6 +1,12 @@
 package hu.hunjam25.dlhc;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import hu.hunjam25.dlhc.sound.Sound;
+import hu.hunjam25.dlhc.sound.SoundBuffer;
+
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -10,51 +16,73 @@ public class AssetManager {
 
     private static HashMap<String, BufferedImage> imageStorage;
 
-    static void init() throws IOException {
-        // TODO: better
+    private static HashMap<String, SoundBuffer> soundStorage;
+
+    private static void img(String name, String pathFirst, String... pathRest) throws IOException {
+        imageStorage.put(name, ImageIO.read(Path.of(pathFirst, pathRest).toFile()));
+    }
+
+    static void init() throws IOException, UnsupportedAudioFileException {
         imageStorage = new HashMap<>();
+        soundStorage = new HashMap<>();
+
+        addSounds();
+
         addProgramerArt();
 
         addProgrammerRemy();
 
-        //addAllWorkSations();
+        // addAllWorkSations();
 
         addWorkStations();
     }
 
     private static void addProgrammerRemy() throws IOException {
-        for (int i = 1; i <= 6; ++i){
-            imageStorage.put("remi" + i,ImageIO.read(Path.of("art","programmerArt","remi", "animalt_remi" + i + ".png").toFile()));
+        for (int i = 1; i <= 6; ++i) {
+            img("remi" + i, "art", "programmerArt", "remi", "animalt_remi" + i + ".png");
         }
     }
 
     private static void addAllWorkSations() throws IOException {
-        imageStorage.put("stove_front", ImageIO.read(Path.of("art","organized", "stove", "stove_front_on.png").toFile()));
-        imageStorage.put("sink_front_on", ImageIO.read(Path.of("art","organized", "sink", "sink_front_on.png").toFile()));
-        imageStorage.put("sink_front_off", ImageIO.read(Path.of("art","organized", "sink", "sink_front_off.png").toFile()));
-        imageStorage.put("counter_front", ImageIO.read(Path.of("art","organized", "counter", "counter_front.png").toFile()));
-        imageStorage.put("sink_back_off", ImageIO.read(Path.of("art","organized", "sink", "sink_behind_off.png").toFile()));
-        imageStorage.put("sink_back_on", ImageIO.read(Path.of("art","organized", "sink", "sink_behind_on.png").toFile()));
-        imageStorage.put("stove_back_off", ImageIO.read(Path.of("art","organized", "stove", "stove_behind_off.png").toFile()));
-        imageStorage.put("stove_back_on", ImageIO.read(Path.of("art","organized", "stove", "stove_behind_on.png").toFile()));
-        imageStorage.put("counter_back", ImageIO.read(Path.of("art","organized", "counter", "counter_behind.png").toFile()));
-        imageStorage.put("fridge_closed", ImageIO.read(Path.of("art","organized", "fridge", "fridge_closed.png").toFile()));
-        imageStorage.put("fridge_open", ImageIO.read(Path.of("art","organized", "fridge", "fridge_open.png").toFile()));
+        img("stove_front", "art", "organized", "stove", "stove_front_on.png");
+        img("sink_front_on", "art", "organized", "sink", "sink_front_on.png");
+        img("sink_front_off", "art", "organized", "sink", "sink_front_off.png");
+        img("counter_front", "art", "organized", "counter", "counter_front.png");
+        img("sink_back_off", "art", "organized", "sink", "sink_behind_off.png");
+        img("sink_back_on", "art", "organized", "sink", "sink_behind_on.png");
+        img("stove_back_off", "art", "organized", "stove", "stove_behind_off.png");
+        img("stove_back_on", "art", "organized", "stove", "stove_behind_on.png");
+        img("counter_back", "art", "organized", "counter", "counter_behind.png");
+        img("fridge_closed", "art", "organized", "fridge", "fridge_closed.png");
+        img("fridge_open", "art", "organized", "fridge", "fridge_open.png");
     }
 
     private static void addWorkStations() throws IOException {
-        imageStorage.put("Stove", ImageIO.read(Path.of("art","organized", "stove", "stove_front_on.png").toFile()));
-        imageStorage.put("Sink", ImageIO.read(Path.of("art","organized", "sink", "sink_front_off.png").toFile()));
-        imageStorage.put("Counter", ImageIO.read(Path.of("art","organized", "counter", "counter_front.png").toFile()));
-        imageStorage.put("Fridge", ImageIO.read(Path.of("art","organized", "fridge", "fridge_closed.png").toFile()));
+        img("Stove", "art", "organized", "stove", "stove_front_on.png");
+        img("Sink", "art", "organized", "sink", "sink_front_off.png");
+        img("Counter", "art", "organized", "counter", "counter_front.png");
+        img("Fridge", "art", "organized", "fridge", "fridge_closed.png");
     }
 
     private static void addProgramerArt() throws IOException {
-        imageStorage.put("rat", ImageIO.read(Path.of("art","gonosz_remi.png").toFile()));
-        imageStorage.put("tiles", ImageIO.read(Path.of("art","programmerArt", "tiles.png").toFile()));
-        imageStorage.put("dot", ImageIO.read(Path.of("art", "programmerArt","dot.png").toFile()) );
-        imageStorage.put("mark", ImageIO.read(Path.of("art","programmerArt","mark.png" ).toFile()));
-        imageStorage.put("chef", ImageIO.read(Path.of("art","programmerArt","chef.png").toFile()));
+        imageStorage.put("rat", ImageIO.read(Path.of("art", "gonosz_remi.png").toFile()));
+        imageStorage.put("tiles", ImageIO.read(Path.of("art", "programmerArt", "tiles.png").toFile()));
+        imageStorage.put("dot", ImageIO.read(Path.of("art", "programmerArt", "dot.png").toFile()));
+        imageStorage.put("mark", ImageIO.read(Path.of("art", "programmerArt", "mark.png").toFile()));
+        imageStorage.put("chef", ImageIO.read(Path.of("art", "programmerArt", "chef.png").toFile()));
+    }
+
+    private static void addSounds() throws IOException, UnsupportedAudioFileException {
+        soundStorage.put("pipe", SoundBuffer.read(Path.of("art", "metal-pipe.wav")));
+
+        Sound s = new Sound(soundStorage.get("pipe"));
+
+        try {
+            var c = s.play();
+            c.drain();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
     }
 
     public static BufferedImage getImage(String name) {
